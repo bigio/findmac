8b78080bfb0ebfaa0afa6af082fa9c7bce203634
Giovanni Bechis <giovanni@bigio.snb.it>
2014-04-30 23:51:47 +0200

    Parser module for oui.txt files

--- oui/parse.pm
+++ oui/parse.pm
@@ -7,5 +7,24 @@ use strict;
 package oui::parse;
 
 sub parse_text() {
+my $oui_text = shift;
+my $count = 0;
+my @a_oui;
 
+my @lines = split (/\n/, $oui_text);
+foreach my $value ( @lines ) {
+	if ( $value =~ /hex/ )  {
+		$a_oui[$count]{'MAC'} = substr($value,2, 8);
+		$a_oui[$count]{'BRAND'} = substr($value, 20);
+		$count++;
+	}
+	if ( $value =~ /\t\t\t/ ) {
+		$a_oui[$count]{'DESCR'} .= substr( $value, 4);
+		$a_oui[$count]{'DESCR'} =~ s/\t//g;
+		$a_oui[$count]{'DESCR'} .= "\n";
+	}
 }
+return @a_oui;
+}
+
+1;
